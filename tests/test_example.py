9134b4c5f22acb7916e0bc4110210cd4049d3426
Kuragin Ivan <curagin.vanya@yandex.ru>
2023-11-20 08:49:57 +0300

    3 lession

---
+++ tests/test_example.py
@@ -0,0 +1,10 @@
+from example import reverse
+
+def test_reverse():
+    assert reverse('Hexlet') == 'telxeH'
+
+def test_reverse_for_empty_string():
+    assert reverse('') == ''
+
+test_reverse()
+test_reverse_for_empty_string()
